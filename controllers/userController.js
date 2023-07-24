16c90a633744a04b28a24139e51a1c9dee25f7f7
Kartikey Jaiswal <kartikeyjaiswal@Kartikeys-MacBook-Pro.local>
2023-07-24 20:06:48 +0530

    created jwt access token for user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 //@desc Register a user
@@ -38,7 +39,32 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: "Login the user"});
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatoryhhhh!");
+    }
+    const user = await User.findOne({email: email});
+    if (user && (await bcrypt.compare(password, user.password))) {
+        // if password matches, we'll provide an access token in the response
+        const accessToken = jwt.sign(
+            {   // payload
+                user: { 
+                    username: user.username,
+                    email: user.email,
+                    id: user.id
+                },
+            }, 
+            process.env.ACCESS_TOKEN_SECRET, 
+            {
+                expiresIn: "5m"
+            }
+        );
+        res.status(200).json({accessToken});
+    } else {
+        res.status(401);
+        throw new Error("Email or password is not valid");
+    }
 });
 
 //@desc Current user information
